d34f3c50a6d819cbc41ddad41cf7668c0130718b
shohagnawaz <snawazdigital@gmail.com>
2026-03-07 17:35:04 +0600

    post

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,7 +3,13 @@ import { postService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
     try {
-        const result = await postService.createPost(req.body)
+        const user = req.user;
+        if (!user) {
+            return res.status(400).json({
+                error: "Unauthorized",
+            });
+        }
+        const result = await postService.createPost(req.body, user.id as string)
         res.status(201).json(result)
     }
     catch(error) {
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import { PostController } from "./post.controller";
+import auth, { UserRole } from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post(
-    "/", 
+    "/",
+    auth(UserRole.USER), 
     PostController.createPost
 );
 
